bbeba68c76db41befe26d53445de68f4cafe0112
Jack Henza <jackhenza@gmail.com>
2023-10-15 19:50:21 -0400

    fetch data from server

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
 //server location: https://best-camper-layouts-server.onrender.com/campers
+const campers = []
+
+
+const fetchCampers = () => {
+    fetch("https://best-camper-layouts-server.onrender.com/campers")
+    .then(resp => resp.json())
+    .then(data => campers.splice(0,0,...data))
+    .catch(error =>{
+        alert("Oh noes! Something went wrong!")
+        console.log(error)
+    })
+}
+
+fetchCampers()
