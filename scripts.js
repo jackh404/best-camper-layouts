7b8939ed523cee4a66cab63fd126c905e38f2dbc
Jack Henza <jackhenza@gmail.com>
2023-10-17 14:17:22 -0400

    create working camper gallery

--- scripts.js
+++ scripts.js
@@ -1,15 +1,23 @@
 //server location: https://best-camper-layouts-server.onrender.com/campers
+//dev server: http://localhost:3000/campers
+const serverURL = "http://localhost:3000/campers"
 
 //global js variables
+const campers = []
 let activeCamper
 let upVote = false
 let downVote = false
 
+
+
 //runs initial fetch to retrieve camper data from server
 const fetchCampers = () => {
-    fetch("https://best-camper-layouts-server.onrender.com/campers")
+    fetch(serverURL)
     .then(resp => resp.json())
-    .then(data => data)
+    .then(data => {
+        campers.splice(0,0,...data)
+        renderCampers(data)
+    })
     .catch(error =>{
         alert("Oh noes! Something went wrong!")
         console.log(error)
@@ -17,12 +25,26 @@ const fetchCampers = () => {
 }
 //shows list & thumbnails of campers in the thumbail pane
 const renderCampers = (campers) => {
+    const camper_thumbs = document.getElementById("camper_thumbs")
     campers.forEach(element => {
-        
+        const newDiv = document.createElement('div')
+        newDiv.classList.add("camperThumbDiv")
+
+        const camperImg = document.createElement('img')
+        camperImg.src = element.layoutPic
+        camperImg.alt = element.manufacturer + " " + element.line + " " + element.model
+        camperImg.classList.add("camperThumb")
+
+        const label = document.createElement('h6')
+        label.textContent = element.line + " " + element.model
+
+        newDiv.append(camperImg)
+        newDiv.append(label)
+        camper_thumbs.append(newDiv)
     });
 }
 //initial call upon page load
-renderCampers(fetchCampers())
+fetchCampers()
 
 //filters camper list according to selected criteria
 const filterCampers = (criterion,value) => {
